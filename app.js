bfc538158e4c82dad1370d359f62ac84647eb4f2
Abhijit J. Patil <107745672+a8hi-patil@users.noreply.github.com>
2023-02-21 12:53:59 +0530

    Module practice

---
+++ app.js
@@ -0,0 +1,7 @@
+const duplicateMathModule = require('./Math.js')
+
+console.log(duplicateMathModule)
+duplicateMathModule.Add(5, 6)
+duplicateMathModule.Division(8, 2)
+duplicateMathModule.Multiplication(8, 2)
+duplicateMathModule.Subtract(8, 2)
